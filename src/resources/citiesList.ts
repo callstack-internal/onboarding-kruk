df74f902e43a4e39813ccfc8d81e6fba0591fab7
Grzegorz Kruk <grzegorz.kruk@callstack.com>
2023-01-10 12:01:37 +0100

    feat: add navigation feat: add home screen feat: add weather store feat: add http weather service feat: add enum helper utilities

---
+++ src/resources/citiesList.ts
@@ -0,0 +1,10 @@
+export enum CITIES_LIST {
+  Paris = 2988507,
+  Madrid = 3117735,
+  LosAngeles = 5368361,
+  Boston = 4930956,
+  NewYork = 5128581,
+  Singapore = 1880252,
+  Cracow = 3094802,
+  Wroclaw = 3081368,
+}
